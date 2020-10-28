194c456e163e4b4ddbb0452352b127365b01866c
giorgoskef98 <giorgos.kefalas98@gmail.com>
2020-10-29 01:25:06 +0200

    Exercise_1 has finished completely!

--- exercise_1.cpp
+++ exercise_1.cpp
@@ -8,18 +8,42 @@ int main(){
 
   srand (time(NULL));
 
+  
   //a
-  ac_int<8,false> a=43 ,b=142;
-  ac_int<9,true> c=-152, d=73;  
-  ac_int<18,true> e = a*b + c*d;
+  ac_int<8,false> a ,b;
+  ac_int<9,true> c, d;  
+  ac_int<18,true> e ;
 
   //b
-  ac_fixed<5,2,false> f=2.25;
-  ac_fixed<5,4,true> g=-6.5;
-  ac_fixed<10,6,true> h = f*g;
+  ac_fixed<5,2,false> f;
+  ac_fixed<5,4,true> g;
+  ac_fixed<10,6,true> h;
+  
+  for(int i=0;i<19;i++){
+    a = rand() % 255;
+    b = rand() % 255;
 
-  std::cout << "INT:" << e << std::endl;
-  std::cout << "Float:" << h << std::endl;
+    c = (rand() % 1023) - 512;
+    d = (rand() % 1023) - 512;
+
+    e = a*b+c*d;
+    
+    std::cout << "a:" << a << ",b:" << b << ",c:" << c << ",d:" << d << std::endl;
+    std::cout << "INT:" << e << std::endl;
+
+    std::cout << "******************************" << std::endl; 
+
+    f = 3.875 * ((((float) rand())/ (float)  RAND_MAX));
+    g = (7.5 - (-8))*((((float) rand())/(float) RAND_MAX)) + (-8);
+    
+    h = f*g;
+    
+    std::cout << "f:" << f << ",g:" << g << std::endl;
+    std::cout << "Float:" << h << std::endl;
+
+    std::cout << "******************************" << std::endl;
+  }
+
 
   return 0;
 
